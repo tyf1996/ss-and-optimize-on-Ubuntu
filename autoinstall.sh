0c0f906101b36c2c92ae28648d27358527a5dc92
tyf1996 <tyf.liking@gmail.com>
2019-02-20 18:57:23 +0800

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,7 +4,23 @@ apt-get install python-pip
 pip install shadowsocks
 touch /etc/shadowsocks.json
 echo -e {\\n\\t'"server":"0.0.0.0",'\\n\\t'"local_address":"127.0.0.1",'\\n\\t'"local_port":1080,'\\n\\t'"port_password":{'\\n\\t\\t'"15677" : "hello!@#",'\\n\\t\\t'"15676" : "hello!@#",'\\n\\t\\t'"15675" : "hello!@#",'\\n\\t\\t'"15674" : "hello!@#",'\\n\\t\\t'"15673" : "hello!@#"'\\n\\t'},'\\n\\t'"timeout":150,'\\n\\t'"method":"rc4-md5",'\\n\\t'"fast_open": true'\\n} > /etc/shadowsocks.json
-wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh
+#wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh
+
+echo "#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+sudo ssserver -c /etc/shadowsocks.json -d start
+exit 0" > /etc/rc.local
+
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
